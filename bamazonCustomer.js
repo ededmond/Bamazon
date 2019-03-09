a4b680c8325088348717ca032c982ff49075ecba
Emma Dedmond <emmadedmond@yahoo.com>
2019-03-08 22:21:31 -0500

    added mysql and inquirer to package.json

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,6 +5,22 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
+    //this is where my password is saved.
     password: process.env.DATABASE_PASSWORD,
     database: "bamazon"
-})
+});
+
+connection.connect(function(error) {
+    if (error) throw error;
+    askToBuy();
+});
+
+function askToBuy() {
+    console.log("Select the Product you would like to Buy: \n\n");
+    connection.query("SELECT * FROM PRODUCTS",function(error,data) {
+        if (error) throw error;
+        console.log(data);
+    });
+}
+
+
